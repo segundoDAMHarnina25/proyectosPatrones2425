2dc3b8d19ee49821628b4207f575b743bd9d30b9
programacion <damharnina25@iesarroyoharnina.es>
2024-10-16 10:51:40 +0200

    update

--- 05Builder/src/ejercicio02/NutritionFactsTest.java
+++ 05Builder/src/ejercicio02/NutritionFactsTest.java
@@ -10,6 +10,9 @@ class NutritionFactsTest {
 				 NutritionFacts.Builder.builder(3,4).calories(2).build();
 		NutritionFactsSingleton nutritionFactsSingleton=
 				 NutritionFactsSingleton.Builder.getInstance(3,4).calories(2).build();
+		NutritionFactsSingleton nutritionFactsSingleton2=
+				NutritionFactsSingleton.Builder.getInstance(3,4).fat(2).build();
+		System.out.println();
 	}
 
 }
